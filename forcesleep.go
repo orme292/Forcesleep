d09160cb902288a8915cd7d494fbbbc651a64c84
orme292 <orme.andrew@gmail.com>
2020-12-21 02:07:25 -0500

    process checking and verbosity
    Splits the process checking and cancel option into separate processes.
    Handle multiple shutdown processes.
    Adds some verbosity.

--- forcesleep.go
+++ forcesleep.go
@@ -5,45 +5,75 @@ import (
 	"os"
 	"os/exec"
 	"strconv"
+	"strings"
 )
 
+func findProcesses(print bool) (err int, result []string) {
+	cmd1 := "ps -ef | grep -i shutdown | grep -v grep | awk '{print $2}'"
+	output, cmderr := exec.Command("bash", "-c", cmd1).Output()
+	var processes []string
+	if cmderr != nil {
+		if print == true {
+			fmt.Println("Failed when searching for processes.")
+		}
+		err = 1
+	} else if len(string(output)) <= 0 {
+		if print == true {
+			fmt.Println("No shutdown process found.")
+		}
+		err = 1
+	} else {
+		processes = strings.Split(string(output), "\n")
+		processes = processes[:len(processes)-1]
+		if print == true {
+			fmt.Println("Found", len(processes), "shutdown processes.")
+		}
+	}
+	return err, processes
+}
+
+func cancelSleep() {
+	err, processes := findProcesses(true)
+	if err != 0 {
+		os.Exit(0)
+	}
+	for _, process := range processes {
+		fmt.Println("Ending process", process)
+		cmd := "sudo kill -9 " + process
+		_, errs := exec.Command("bash", "-c", cmd).Output()
+		if errs != nil {
+			fmt.Println(errs)
+			os.Exit(0)
+		}
+	}
+	err, processes = findProcesses(true)
+	os.Exit(0)
+}
+
 func main() {
-
 	var args string
-
+	fmt.Println("")
 	if len(os.Args) > 1 {
 		args = os.Args[1]
 	} else {
 		fmt.Println("You have to specify the delay in minutes or 'cancel'.")
 		os.Exit(0)
 	}
-
-	if args == "cancel" {
-		cmd1 := "ps -ef | grep -i shutdown | grep -v grep | awk '{print $2}'"
-		out, err := exec.Command("bash", "-c", cmd1).Output()
-		if err != nil {
-			fmt.Println("Failed.")
-			os.Exit(0)
-		}
-		if len(string(out)) <= 0 {
-			fmt.Println("No shutdown process found.")
-			os.Exit(0)
-		}
-		fmt.Println("Ending shutdown process " + string(out))
-		cmd2 := "sudo kill -9 " + string(out)
-		out, err = exec.Command("bash", "-c", cmd2).Output()
-		fmt.Println(string(out))
-		os.Exit(0)
+	if strings.ToLower(args) == "cancel" {
+		cancelSleep()
 	}
-
 	_, err := strconv.Atoi(args)
 	if err != nil {
 		fmt.Println("You have to provide a number of minutes.")
 		os.Exit(0)
 	}
-
+	_, processes := findProcesses(false)
+	if len(processes) > 0 {
+		fmt.Println("")
+		fmt.Println("**A shutdown process already exists. Another will be created.**")
+		fmt.Println("- Use 'forcesleep cancel' to cancel.")
+	}
 	cmd := exec.Command("sudo", "shutdown", "-s", "+"+args)
-
 	err = cmd.Run()
 	if err != nil {
 		fmt.Println("Could not execute command.\n", err)
